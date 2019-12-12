d641f75041fea0d3acc4d26962071772fbfaa597
vbvg2008 <shawnmengdong@gmail.com>
2019-12-12 15:45:05 -0700

    added misc files

---
+++ fastestimator/cli/train.py
@@ -0,0 +1,81 @@
+# Copyright 2019 The FastEstimator Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+import argparse
+import json
+import os
+import sys
+
+from fastestimator.util.util import parse_string_to_python
+
+
+def parse_cli_to_dictionary(input_list):
+    """
+    Args:
+        input_list: A list of input strings from a cli
+
+    Returns:
+        A dictionary constructed from the input list, with values converted to python objects where applicable
+    """
+    result = {}
+    if input_list is None:
+        return result
+    key = ""
+    val = ""
+    idx = 0
+    while idx < len(input_list):
+        if input_list[idx].startswith("--"):
+            if len(key) > 0:
+                result[key] = parse_string_to_python(val)
+            val = ""
+            key = input_list[idx].strip('--')
+        else:
+            val += input_list[idx]
+        idx += 1
+    if len(key) > 0:
+        result[key] = parse_string_to_python(val)
+    return result
+
+
+def train(args, unknown):
+    entry_point = args['entry_point']
+    hyperparameters = {}
+    if args['hyperparameters_json']:
+        hyperparameters = os.path.abspath(args['hyperparameters_json'])
+        hyperparameters = json.load(open(hyperparameters, 'r'))
+    hyperparameters.update(parse_cli_to_dictionary(unknown))
+    module_name = os.path.splitext(os.path.basename(entry_point))[0]
+    dir_name = os.path.abspath(os.path.dirname(entry_point))
+    sys.path.insert(0, dir_name)
+    spec_module = __import__(module_name, globals(), locals(), ["get_estimator"])
+    estimator = spec_module.get_estimator(**hyperparameters)
+    estimator.fit()
+
+
+def configure_train_parser(subparsers):
+    parser = subparsers.add_parser('train',
+                                   description='Train a FastEstimator model',
+                                   formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+                                   allow_abbrev=False)
+    # use an argument group for required flag arguments since otherwise they will show up as optional in the help
+    parser.add_argument('entry_point', type=str, help='The path to the model python file')
+    parser.add_argument('--hyperparameters',
+                        dest='hyperparameters_json',
+                        type=str,
+                        help="The path to the hyperparameters JSON file")
+    parser.add_argument_group(
+        'hyperparameter arguments',
+        'Arguments to be passed through to the get_estimator() call. \
+        Examples might look like --epochs <int>, --batch_size <int>, --optimizer <str>, etc...')
+    parser.set_defaults(func=train)
---
+++ fastestimator/util/util.py
@@ -0,0 +1,35 @@
+# Copyright 2019 The FastEstimator Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Utilities for FastEstimator."""
+import json
+from ast import literal_eval
+
+
+def parse_string_to_python(val):
+    """
+    Args:
+        val: An input string
+    Returns:
+        A python object version of the input string
+    """
+    if val is None or not val:
+        return ""
+    try:
+        return literal_eval(val)
+    except (ValueError, SyntaxError):
+        try:
+            return json.loads(val)
+        except json.JSONDecodeError:
+            return val
